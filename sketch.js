e0e0905fa58ade806b5af2de26be3d964cf02fb5
KurtYilmaz <kurtyilmaz@ufl.edu>
2019-11-26 22:50:00 -0500

    Fixed square and sawtooth waves

--- sketch.js
+++ sketch.js
@@ -1,21 +1,56 @@
 let xSpacing = 16; // Distance between each horizontal location
-let ySpacing = 120; // Distance between each wave
+let ySpacing; // Distance between each wave
 let waveWidth; // Width of entire wave
-let theta = 0.0; // Start angle at 0
-let amplitude = 30.0; // Height of wave
+let theta = 0; // Start angle at 0
+let amplitude; // Height of wave
 let frequency = 0.0033; // wave frequency
 let dx; // Value for incrementing x
 let sinValues; // Using an array to store height values for the wave
-let padding = 30; // Padding from top
+let padding; // Padding from top
+let renderLines = false; // flag for rendering lines between poitns
+let pointWidth = 8; // Width of the wave ellipses
+let speed = 0.02; // Speed of wave scrolling
+let dropThreshold;
+let dropSpeed = 18; // Speed of saw and square points dropping
+p5.disableFriendlyErrors = true; // Performance optimization
+let currMils = 0;
+let prevMils = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  waveWidth = width + 16;
   dx = TWO_PI * frequency * xSpacing;
+  initConstants();
   sinValues = new Array(floor(waveWidth / xSpacing));
   squareValues = new Array(floor(waveWidth / xSpacing));
   triangleValues = new Array(floor(waveWidth / xSpacing));
   sawValues = new Array(floor(waveWidth / xSpacing));
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  initConstants();
+  sinValues = new Array(floor(waveWidth / xSpacing));
+  squareValues = new Array(floor(waveWidth / xSpacing));
+  triangleValues = new Array(floor(waveWidth / xSpacing));
+  sawValues = new Array(floor(waveWidth / xSpacing));
+}
+
+function initConstants() {
+  waveWidth = width + 16;
+  ySpacing = windowHeight / 4;
+  padding = windowHeight / 20;
+  amplitude = windowHeight / 20;
+  dropThreshold = 0.97 * PI;
+}
+
+function timerStart() {
+  prevMils = millis();
+}
+
+function timerPrint(id) {
+  currMils = millis() - prevMils;
+  prevMils = currMils;
+  print(currMils * 1000 + "- " + id + "\n");
 }
 
 function draw() {
@@ -25,7 +60,7 @@ function draw() {
   calcTriangle(theta);
   calcSaw(theta);
   renderWaves();
-  theta += 0.02 % TWO_PI;
+  theta += speed % TWO_PI;
 }
 
 function calcSin(thetaSin) {
@@ -38,9 +73,19 @@ function calcSin(thetaSin) {
 }
 
 function calcSquare(thetaSquare) {
-  let x = thetaSquare;
+  let x = thetaSquare % TWO_PI;
   for (let i = 0; i < squareValues.length; i++) {
-    squareValues[i] = x > PI ? amplitude : -amplitude;
+    if (x > dropThreshold && x < PI) {
+      squareValues[i] += speed * dropSpeed * amplitude;
+      squareValues[i] = Math.min(squareValues[i], amplitude);
+    } else if (x > dropThreshold * 2 && x < TWO_PI) {
+      squareValues[i] += -speed * dropSpeed * amplitude;
+      squareValues[i] = Math.max(squareValues[i], -amplitude);
+    } else if (x >= PI) {
+      squareValues[i] = amplitude;
+    } else {
+      squareValues[i] = -amplitude;
+    }
     x = (x + dx) % TWO_PI;
   }
 }
@@ -66,7 +111,10 @@ function calcSaw(thetaSaw) {
   let x = thetaSaw % TWO_PI;
   let slope = amplitude / PI;
   for (let i = 0; i < sawValues.length; i++) {
-    if (x < PI) {
+    if (x > dropThreshold && x < PI) {
+      sawValues[i] += speed * dropSpeed * amplitude;
+      sawValues[i] = Math.min(squareValues[i], amplitude);
+    } else if (x <= PI) {
       sawValues[i] = -(slope * x);
     } else {
       sawValues[i] = -(slope * x - 2 * amplitude);
@@ -77,131 +125,78 @@ function calcSaw(thetaSaw) {
 }
 
 function renderWaves() {
+  renderSin(padding + amplitude, "#1abc9c");
+  renderSquare(padding + amplitude + ySpacing, "#bc1a7b");
+  renderTriangle(padding + amplitude + 2 * ySpacing, "#bc3a1a");
+  renderSaw(padding + amplitude + 3 * ySpacing, "#bc8b1a");
+}
+
+function renderSin(y, waveColor) {
   strokeWeight(6);
   // A simple way to draw the wave with an ellipse at each location
   for (let x = 0; x < sinValues.length; x++) {
-    fill(color("#1abc9c"));
-    stroke(color("#1abc9c"));
-    ellipse(x * xSpacing, padding + amplitude + sinValues[x], 8, 8);
-    if (x > 0) {
+    fill(color(waveColor));
+    stroke(color(waveColor));
+    ellipse(x * xSpacing, sinValues[x] + y, pointWidth, pointWidth);
+    if (x > 0 && renderLines) {
       line(
         (x - 1) * xSpacing,
-        sinValues[x - 1] + padding + amplitude,
+        sinValues[x - 1] + y,
         x * xSpacing,
-        sinValues[x] + padding + amplitude
+        sinValues[x] + y
       );
     }
-    //line(0, padding + amplitude, width, padding + amplitude);
+  }
+}
 
-    fill(color("#bc1a7b"));
-    stroke(color("#bc1a7b"));
-    ellipse(
-      x * xSpacing,
-      padding + amplitude + ySpacing + squareValues[x],
-      8,
-      8
-    );
-    if (x > 0) {
-      if (squareValues[x - 1] != squareValues[x]) {
-        ellipse(
-          (x - 1) * xSpacing,
-          padding + amplitude + ySpacing + squareValues[x],
-          8,
-          8
-        );
+function renderSquare(y, waveColor) {
+  for (let x = 0; x < sinValues.length; x++) {
+    fill(color(waveColor));
+    stroke(color(waveColor));
+    ellipse(x * xSpacing, squareValues[x] + y, pointWidth, pointWidth);
+    if (x > 0 && renderLines) {
+      if (squareValues[x - 1] == squareValues[x]) {
         line(
           (x - 1) * xSpacing,
-          squareValues[x - 1] + padding + amplitude + ySpacing,
-          (x - 1) * xSpacing,
-          squareValues[x] + padding + amplitude + ySpacing
-        );
-        line(
-          (x - 1) * xSpacing,
-          squareValues[x] + padding + amplitude + ySpacing,
+          squareValues[x - 1] + y,
           x * xSpacing,
-          squareValues[x] + padding + amplitude + ySpacing
-        );
-      } else {
-        line(
-          (x - 1) * xSpacing,
-          squareValues[x - 1] + padding + amplitude + ySpacing,
-          x * xSpacing,
-          squareValues[x] + padding + amplitude + ySpacing
+          squareValues[x] + y
         );
       }
     }
-    // line(
-    //   0,
-    //   padding + amplitude + ySpacing,
-    //   width,
-    //   padding + amplitude + ySpacing
-    // );
+  }
+}
 
-    fill(color("#bc3a1a"));
-    stroke(color("#bc3a1a"));
-    ellipse(
-      x * xSpacing,
-      padding + amplitude + 2 * ySpacing + triangleValues[x],
-      8,
-      8
-    );
-    if (x > 0) {
+function renderTriangle(y, waveColor) {
+  for (let x = 0; x < sinValues.length; x++) {
+    fill(color(waveColor));
+    stroke(color(waveColor));
+    ellipse(x * xSpacing, triangleValues[x] + y, pointWidth, pointWidth);
+    if (x > 0 && renderLines) {
       line(
         (x - 1) * xSpacing,
-        triangleValues[x - 1] + padding + amplitude + 2 * ySpacing,
+        triangleValues[x - 1] + y,
         x * xSpacing,
-        triangleValues[x] + padding + amplitude + 2 * ySpacing
+        triangleValues[x] + y
       );
     }
-    // line(
-    //   0,
-    //   padding + amplitude + 2 * ySpacing,
-    //   width,
-    //   padding + amplitude + 2 * ySpacing
-    // );
+  }
+}
 
-    fill(color("#bc8b1a"));
-    stroke(color("#bc8b1a"));
-    ellipse(
-      x * xSpacing,
-      padding + amplitude + 3 * ySpacing + sawValues[x],
-      8,
-      8
-    );
-    if (x > 0) {
-      if (abs(sawValues[x - 1] - sawValues[x]) >= amplitude) {
-        ellipse(
-          (x - 1) * xSpacing,
-          padding + amplitude + 3 * ySpacing + sawValues[x - 1] + 2 * amplitude,
-          8,
-          8
-        );
+function renderSaw(y, waveColor) {
+  for (let x = 0; x < sinValues.length; x++) {
+    fill(color(waveColor));
+    stroke(color(waveColor));
+    ellipse(x * xSpacing, sawValues[x] + y, pointWidth, pointWidth);
+    if (x > 0 && renderLines) {
+      if (abs(sawValues[x - 1] - sawValues[x]) <= amplitude * 0.15) {
         line(
           (x - 1) * xSpacing,
-          sawValues[x - 1] + padding + amplitude + 3 * ySpacing,
-          (x - 1) * xSpacing,
-          sawValues[x - 1] + 2 * amplitude + padding + amplitude + 3 * ySpacing
-        );
-        line(
-          (x - 1) * xSpacing,
-          sawValues[x - 1] + 2 * amplitude + padding + amplitude + 3 * ySpacing,
+          sawValues[x - 1] + y,
           x * xSpacing,
-          sawValues[x] + padding + amplitude + 3 * ySpacing
-        );
-      } else {
-        line(
-          (x - 1) * xSpacing,
-          sawValues[x - 1] + padding + amplitude + 3 * ySpacing,
-          x * xSpacing,
-          sawValues[x] + padding + amplitude + 3 * ySpacing
+          sawValues[x] + y
         );
       }
     }
-    // line(
-    //   0,
-    //   padding + amplitude + 3 * ySpacing,
-    //   width,
-    //   padding + amplitude + 3 * ySpacing
-    // );
   }
 }
